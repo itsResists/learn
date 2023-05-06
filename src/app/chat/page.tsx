a5c16817d5545612cc5977ca90f7cf73d6fd300d
Lone <bcstolfi@gmail.com>
2023-05-06 17:31:41 -0400

    Initial commit with basic user auth/register

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default async function Chat() {
+
+    return (
+        <main >
+            <div            >
+                <h1>HELLO FRIENDS</h1>
+            </div>
+        </main>
+    );
+}
